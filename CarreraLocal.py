afdc027d2e6e2e6cf2c08ac885c0fc2d2d8413be
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-08-08 10:38:21 -0600

    se termino de agregar la opcion 3 ademas de corregir alguno errores en el codigo

--- CarreraLocal.py
+++ CarreraLocal.py
@@ -1,25 +1,27 @@
-def quick_sort(lista,clave):
+def quick_sort(lista, clave):
     if len(lista) <= 1:
         return lista
 
     pivote = lista[0]
-    mayores = [x for x in lista[1:] if x[1][clave] > pivote[1][clave]]
     menores = [x for x in lista[1:] if x[1][clave] < pivote[1][clave]]
+    mayores = [x for x in lista[1:] if x[1][clave] > pivote[1][clave]]
 
-    return quick_sort(menores,clave)+[pivote]+quick_sort(mayores,clave)
-
+    return quick_sort(menores, clave) + [pivote] + quick_sort(mayores, clave)
 
 
 carrera = {}
 opcion = 0
-while opcion !=4:
+while opcion != 4:
     print("--- MENÚ DE OPCIONES ---")
     print("1. Agregar participante")
     print("2. Mostrar participantes ordenados por nombre")
     print("3. Mostrar participantes ordenados por edad")
     print("4. Salir")
-    opcion = int(input("Seleccione una opcion: "))
-
+    try:
+        opcion = int(input("Seleccione una opcion: "))
+    except ValueError:
+        print("Opcion no valida")
+        continue
     match opcion:
         case 1:
             print("=== Registrar participante ===")
@@ -30,22 +32,33 @@ while opcion !=4:
                 continue
 
             for j in range(cantidad):
-                print(f"Participante {j+1}")
+                print(f"Participante {j + 1}")
                 dorsal = int(input("Numero de Dorsal: "))
                 nombre = input("Nombre del participante: ")
                 edad = int(input("Edad: "))
                 categoria = input("Categoria(Juvenil,adulto,master): ")
-                carrera[dorsal]={
+                carrera[dorsal] = {
                     "nombre": nombre,
                     "edad": edad,
                     "categoria": categoria
                 }
 
         case 2:
-            corredores= list(carrera.items())
+            corredores = list(carrera.items())
             print("=== Mostrar participante ordenado por nombre ===")
-            resultado = quick_sort(corredores,"nombre")
-            for dorsales,datos in resultado:
-                print(f" {datos['nombre']} (Dorsal {dorsales}, Edad: {datos['edad']}, Categoria: {datos['categoria']})")
+            resultado = quick_sort(corredores, "nombre")
+            for dorsales, datos in resultado:
+                print(f" {datos['nombre']} (Dorsal {dorsales}, "
+                      f"Edad: {datos['edad']}, Categoria: {datos['categoria']})")
 
+        case 3:
+            corredores = list(carrera.items())
+            print("=== Mostrar participante ordenado por edad ===")
+            resultado = quick_sort(corredores, "edad")
+            for dorsales, datos in resultado:
+                print(f"Edad {datos['edad']} {datos['nombre']}"
+                      f" Dorsal {dorsales} Categoria {datos['categoria']}")
 
+        case 4:
+            print("=== Salir ===")
+            print("saliendo del programa")
